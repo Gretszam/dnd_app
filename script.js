0c50e988c9a6aa371d02e235e4fa8dec009a79c3
Gretszam <penguino11111@gmail.com>
2020-05-07 22:53:41 -0700

    idk qhat im doing but list! and new JS

---
+++ script.js
@@ -0,0 +1,15 @@
+function addItem(){
+    var ul = document.getElementById("dynamic-list");
+    var inventory = document.getElementById("inventory");
+    var li = document.createElement("li");
+    li.setAttribute('id',inventory.value);
+    li.appendChild(document.createTextNode(inventory.value));
+    ul.appendChild(li);
+}
+
+function removeItem(){
+    var ul = document.getElementById("dynamic-list");
+    var inventory = document.getElementById("inventory");
+    var item = document.getElementById(inventory.value);
+    ul.removeChild(item);
+}
