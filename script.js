1f0e8a5b16c268fc0bca69822e9df2127eab2868
Gretszam <penguino11111@gmail.com>
2020-05-07 23:21:55 -0700

    stats page update

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-function addItem(){
-    var ul = document.getElementById("dynamic-list");
-    var inventory = document.getElementById("inventory");
-    var li = document.createElement("li");
-    li.setAttribute('id',inventory.value);
-    li.appendChild(document.createTextNode(inventory.value));
-    ul.appendChild(li);
-}
+Features:
+    function addItem() {
+        var ul = document.getElementById("dynamic-list");
+        var inventory = document.getElementById("inventory");
+        var li = document.createElement("li");
+        li.setAttribute('id', inventory.value);
+        li.appendChild(document.createTextNode(inventory.value));
+        ul.appendChild(li);
+    }
 
-function removeItem(){
+function removeItem() {
     var ul = document.getElementById("dynamic-list");
     var inventory = document.getElementById("inventory");
     var item = document.getElementById(inventory.value);
     ul.removeChild(item);
 }
+
+Stats:
+    let character = {
+        name: "",
+        ac: 10,
+        int: 0,
+        speed: 30,
+    };
+
+character.name = "Blob";
+character.ac = 12;
+character.int = 2;
+
+$(document).ready(() => {
+    $output = $("#output");
+    $output.append(
+        $("<p></p>").text(character.name),
+        $("<p></p>").text("AC: " + character.ac),
+        $("<p></p>").text("Initiative: " + character.int),
+        $("<p></p>") text.("Speed: " + character.speed),
+    );
+});
