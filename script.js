6216ed5ca11c0996896eaa95b441fee9b99d9259
Gretszam <penguino11111@gmail.com>
2020-05-08 17:15:10 -0700

    updates

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ Features:
         li.setAttribute('id', inventory.value);
         li.appendChild(document.createTextNode(inventory.value));
         ul.appendChild(li);
-    }
+    };
 
 function removeItem() {
     var ul = document.getElementById("dynamic-list");
     var inventory = document.getElementById("inventory");
     var item = document.getElementById(inventory.value);
     ul.removeChild(item);
-}
+};
 
 Stats:
     let character = {
