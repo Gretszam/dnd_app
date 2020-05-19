324de174d4b4cc6dd8b48c50aa88b89c765f23cf
Gretszam <penguino11111@gmail.com>
2020-05-18 20:19:13 -0700

    tried making max hp roll work

--- featuresScript.js
+++ featuresScript.js
@@ -1,4 +1,21 @@
-   var select = document.getElementById("classDrop");
+   function addItem() {
+       var ul = document.getElementById("dynamic-list");
+       var inventory = document.getElementById("inventory");
+       var li = document.createElement("li");
+       li.setAttribute('id', inventory.value);
+       li.appendChild(document.createTextNode(inventory.value));
+       ul.appendChild(li);
+   }
+
+   function removeItem() {
+       var ul = document.getElementById("dynamic-list");
+       var inventory = document.getElementById("inventory");
+       var item = document.getElementById(inventory.value);
+       ul.removeChild(item);
+   }
+
+
+var select = document.getElementById("classDrop");
 
    var classes = ["Fighter", "Paladin", "Wizard"];
 
